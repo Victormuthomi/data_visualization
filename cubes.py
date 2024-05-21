da881e0339f63590747a08ab2531559835fedba3
Victormuthomi <victormuhomi100@gmail.com>
2024-05-22 01:17:16 +0300

    Modify cubes.py.

--- cubes.py
+++ cubes.py
@@ -5,5 +5,18 @@ y_values = ([x **3 for x in x_values])
 
 fig, ax = plt.subplots()
 
-ax.scatter(x_values, y_values, c = y_values, cmp = plt.cm.Blues, s=10)
+ax.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues, s=100)
+
+#set the title and axis labels
+ax.set_title("Cubes", fontsize = 14)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Value of cubes", fontsize = 14)
+
+#set the tick labelsiize
+ax.tick_params(labelsize=14)
+ax.axis([0,100, 0, 1_100_000])
+
+plt.show()
+
+plt.savefig("cubes.png", bbox_inches="tight")
 
