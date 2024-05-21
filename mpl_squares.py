3aec849a3b7e22c441a4b8f8f638c9a426cfdde8
Victormuthomi <victormuhomi100@gmail.com>
2024-05-21 23:13:32 +0300

    Modify mpl_squares.py.

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 
-input_values =[1, 4, 8, 15,25
+input_values =[1, 4, 8, 15,25]
 squares = [1, 4, 8, 16, 25]
+
+plt.style.use('seaborn-v0_8-darkgrid')
 
 fig, ax = plt.subplots()
 # Increase the line width
 ax.plot(squares)
+
 
 #set the title and label for  the y and x axis
 ax.set_title("Squares", fontsize =14)
