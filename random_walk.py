81bd5f38f06388dfde93a7870ac1e6f5874134c8
Victormuthomi <victormuhomi100@gmail.com>
2024-05-23 23:03:01 +0300

    Modify random walk class.

--- random_walk.py
+++ random_walk.py
@@ -21,18 +21,19 @@ class RandowWalks:
             y_direction = choice([1, -1])
             y_distance= ([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance 
-
+            
             #Reject moves that go nowhere
-            if x_step == o and y_step == 0:
+            if x_step == 0 and y_step == 0:
                 continue
 
-           #Calculate the final moves
-           x = self.x_values[-1] + x_step
-           y = self.y_values[-1] + y_step 
+            #Calculate the final moves
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
 
-           self.x_values.append(x)
-           self.y_values.append(y)
+            self.x_values.append(x)
+            self.y_values.append(y)
            
+
 
         
 
