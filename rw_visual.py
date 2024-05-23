43b14766f47c0485b8483710537da2a6417886b7
Victormuthomi <victormuhomi100@gmail.com>
2024-05-24 00:19:02 +0300

    Add a while loop to generate many random walks.

--- rw_visual.py
+++ rw_visual.py
@@ -4,15 +4,18 @@ from random_walk import RandomWalk
 
 #make a random walk 
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    #plot the random work points
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s = 15)
+    ax.set_aspect = ('equal')
+    plt.show()
 
-#plot the random work points
-plt.style.use('classic')
+    keep_running = input("Make another walk(y/n): ")
+    if keep_running == 'n':
+        break 
 
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s = 15)
-ax.set_aspect = ('equal')
-
-plt.show()
 
